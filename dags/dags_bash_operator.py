9fe74a466401a7d1d804b0ac5e43acaafca61ffc
Jongsik Kim <jfreshik@gmail.com>
2025-04-02 00:31:47 +0900

    add bash_operator

---
+++ dags/dags_bash_operator.py
@@ -0,0 +1,30 @@
+import datetime
+
+import pendulum
+
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
+
+with DAG(
+    dag_id="dags_bash_operator",
+    schedule="0 0 * * *",
+    start_date=pendulum.datetime(2025, 4, 1, tz="Asia/Seoul"),
+    catchup=False,
+    dagrun_timeout=datetime.timedelta(minutes=60),
+    tags=["example", "example2", "bash operator"],
+) as dag:
+    
+    # [START howto_operator_bash]
+    bash_t1 = BashOperator(
+        task_id="bash_t1",
+        bash_command="echo whoami",
+    )
+
+    bash_t2 = BashOperator(
+        task_id="bash_t2",
+        bash_command="echo $HOSTNAME",
+    )
+    # [END howto_operator_bash]
+
+    bash_t1 >> bash_t2
